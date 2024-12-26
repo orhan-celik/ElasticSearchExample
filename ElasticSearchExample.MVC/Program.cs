dd7dd1f5bf787210dcccdfac2511a6c38e20c05c
orhan-celik <orhancelik@windowslive.com>
2024-12-26 16:34:08 +0300

    Elastic.Clients.Elasticsearch kütüphanesi kuruldu ve DI Container a eklendi.

--- ElasticSearchExample.MVC/Program.cs
+++ ElasticSearchExample.MVC/Program.cs
@@ -1,15 +1,18 @@
+using ElasticSearchExample.MVC.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Elastic Search Singleton olarak servislerin arasýna ekliyoruz.
+builder.Services.AddElasticSearch(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
