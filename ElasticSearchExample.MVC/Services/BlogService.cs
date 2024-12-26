e3375c634213ebc3fa267afdde72e3e8a2a90a88
orhan-celik <orhancelik@windowslive.com>
2024-12-26 18:34:59 +0300

    Blog kayıt etme ve listeleme işlemleri tamamlandı.

---
+++ ElasticSearchExample.MVC/Services/BlogService.cs
@@ -0,0 +1,42 @@
+﻿using ElasticSearchExample.MVC.Models;
+using ElasticSearchExample.MVC.Repositories;
+using ElasticSearchExample.MVC.ViewModels;
+
+namespace ElasticSearchExample.MVC.Services
+{
+    public class BlogService
+    {
+        private readonly BlogRepository _blogRepository;
+
+        public BlogService(BlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<BlogListViewModel>> GetAllAsync()
+        {
+            var result = await _blogRepository.GetAllAsync();
+            var viewResult = result.Select(x => new BlogListViewModel
+            {
+                Content = x.Content,
+                Title = x.Title,
+                Created = x.Created.ToShortDateString() ?? "N/A", // Null kontrolü eklendi
+                Tags = x.Tags != null ? string.Join(", ", x.Tags) : "No Tags" // Tags birleştirilerek dönüştürüldü
+            }).ToList();
+            return viewResult;
+        }
+
+        public async Task<bool> SaveAsync(BlogCreateViewModel model)
+        {
+            var newBlog = new Blog
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Tags = model.Tags.Split(","),
+                UserId = Guid.NewGuid()
+            };
+            var result = await _blogRepository.SaveAsync(newBlog);
+            return result != null;
+        }
+    }
+}
---
+++ ElasticSearchExample.MVC/ViewModels/BlogCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElasticSearchExample.MVC.ViewModels
+{
+    public class BlogCreateViewModel
+    {
+        [Display(Name = "Blog Başlık")]
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Display(Name = "Blog İçerik")]
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Display(Name = "Blog Tagları")]
+        [Required]
+        public string Tags { get; set; } = null!;
+    }
+}
